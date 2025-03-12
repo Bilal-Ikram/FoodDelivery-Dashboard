74fad479fcb79250f1823ffaed1ac8ff182bcb0a
bilal2-png <bialik3210@gmail.com>
2025-03-13 01:17:55 +0500

    Enhance Restaurant Routes: Add endpoints for creating menu items and updating menu item images

--- rl/backend/src/routes/restaurantRoutes.js
+++ rl/backend/src/routes/restaurantRoutes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-import { registerRestaurant } from "../controllers/user.controller.js";
-import { loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { registerRestaurant } from "../controllers/user.controller.js";
+import { createMenuItem, updateMenuItemImage } from '../controllers/menu.controller.js';
+import { loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+
 const router = Router();
 
 // Register a new restaurant
@@ -11,5 +13,9 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+
+// Create a new menu item
+router.post('/restaurants/:restaurantId/menu', createMenuItem);
+router.patch('/menu/:id/image', updateMenuItemImage);
 
 export default router;
