c8d55bee79fc143711a3771d2af48d8f05e5157c
Bilal Ikram <126901173+Bilal-Ikram@users.noreply.github.com>
2025-04-21 17:10:24 +0500

    Delete rl directory

--- rl/frontend/src/components/Signup/constants/formFields.jsx
+++
@@ -1,9 +0,0 @@
-// src/constants/formFields.js
-export const FORM_FIELDS = [
-    { name: 'restaurantName', label: 'Restaurant Name', type: 'text' },
-    { name: 'fullName', label: 'FullName', type: 'text' },
-    { name: 'email', label: 'Email', type: 'email' },
-    { name: 'password', label: 'Password', type: 'password' },
-    { name: 'phone', label: 'Phone', type: 'tel' },
-    { name: 'address', label: 'Restaurant Address', type: 'text' }
-  ];
